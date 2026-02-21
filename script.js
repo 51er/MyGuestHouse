24941a33cc187c544adf0656aeeb418839204f33
51er <2321599@chester.ac.uk>
2026-02-21 20:36:59 +0000

    Update script.js

--- script.js
+++ script.js
@@ -7,4 +7,27 @@ function closeMenu(){
     document.getElementById("sidebar").classList.remove("active");
     document.getElementById("overlay").classList.remove("active");
 }
-
+function simpleValidate(event) {
+    event.preventDefault();
+    
+    let name = document.getElementById('simpleName').value;
+    let email = document.getElementById('simpleEmail').value;
+    let message = document.getElementById('simpleMessage').value;
+    let msgBox = document.getElementById('simpleFormMessage');
+    
+    if (name === "" || email === "" || message === "") {
+        alert("Please fill all fields!");  //Pop out error message
+        return false;
+    }
+    
+//if there is not @
+    if (email.indexOf("@") === -1) {
+        alert("Please enter a valid email!");
+        return false;
+    }
+    
+    alert("Thank you! Message sent.");  // when successfuly sended
+    document.getElementById('simpleContactForm').reset();
+    
+    return false;
+}
