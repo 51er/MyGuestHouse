4441acd42f7d209267ac75d61570ac68867534a7
51er <2321599@chester.ac.uk>
2026-02-22 02:22:56 +0000

    Update script.js

--- script.js
+++ script.js
@@ -31,6 +31,15 @@ function simpleValidate(event) {
     
     return false;
 }
-// References:
-// W3Schools. (2026). JavaScript Tutorial. https://www.w3schools.com/js/
-// MDN Web Docs. (2026). JavaScript Functions. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Functions
+// References
+// W3Schools. (2026). JavaScript form validation. Retrieved February 22, 2026, 
+//   from https://www.w3schools.com/js/js_validation.asp
+//
+// W3Schools. (2026). JavaScript popup boxes. Retrieved February 22, 2026, 
+//   from https://www.w3schools.com/js/js_popup.asp
+//
+// MDN Web Docs. (2026). Event.preventDefault(). Retrieved February 22, 2026, 
+//   from https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
+//
+// MDN Web Docs. (2026). String.prototype.indexOf(). Retrieved February 22, 2026,
+//   from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/indexOf
